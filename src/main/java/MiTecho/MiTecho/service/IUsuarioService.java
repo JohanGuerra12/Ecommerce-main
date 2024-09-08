f2a3790a70109c7c23a00bdea53b7cae1e32fcd2
JohanGuerra12 <167925229+JohanGuerra12@users.noreply.github.com>
2024-09-08 00:46:42 -0500

    Obtener usuario login y sesion

--- src/main/java/MiTecho/MiTecho/service/IUsuarioService.java
+++ src/main/java/MiTecho/MiTecho/service/IUsuarioService.java
@@ -7,5 +7,6 @@ import MiTecho.MiTecho.model.Usuario;
 public interface IUsuarioService {
 	Optional<Usuario> findById(Integer id);
 	Usuario save(Usuario usuario);
+	Optional<Usuario> findByEmail(String email);
 		
 }
