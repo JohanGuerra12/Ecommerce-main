e2c57618b333efa32a12c1341ff664a89d1c6eff
JohanGuerra12 <167925229+JohanGuerra12@users.noreply.github.com>
2025-03-02 21:20:18 -0500

    Update UploadFileService.java

--- src/main/java/MiTecho/MiTecho/service/UploadFileService.java
+++ src/main/java/MiTecho/MiTecho/service/UploadFileService.java
@@ -11,23 +11,28 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class UploadFileService {
- 
-	private String folder="images//";
-	
-	public String saveImage(MultipartFile file) throws IOException {
-		if(!file.isEmpty()) {
-			byte[] bytes=file.getBytes();
-			Path path = Paths.get(folder+file.getOriginalFilename());
-			Files.write(path, bytes);
-			return file.getOriginalFilename();
-			}
-		
-		
-			return "default.jpg";
-	}
-	public void deleteImage(String nombre) {
-		String ruta="images//";
-		File file= new File(ruta+nombre);
-		file.delete();
-	}
+
+    private String folder = System.getProperty("user.dir") + "/uploads/";
+
+    public String saveImage(MultipartFile file) throws IOException {
+        if (!file.isEmpty()) {
+            File dir = new File(folder);
+            if (!dir.exists()) {
+                dir.mkdirs(); // Crea la carpeta si no existe
+            }
+
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(folder + file.getOriginalFilename());
+            Files.write(path, bytes);
+            return file.getOriginalFilename();
+        }
+        return "default.jpg";
+    }
+
+    public void deleteImage(String nombre) {
+        File file = new File(folder + nombre);
+        if (file.exists()) {
+            file.delete();
+        }
+    }
 }
